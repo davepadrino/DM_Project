24834d9695f468ecb86c69706ee623df7cc81440
Davee <Davee>
2016-05-03 10:36:26 -0430

    from win8

---
+++ README.md
@@ -0,0 +1,28 @@
+# DM_Project
+Data Mining Project: Music Recomendator
+
+# Tasks
+
+## Reducir la matriz
+
+## definir una forma de seleccionar sample y test
+
+## 1 - Metricas de similaridad
+
+## 2 - K-Vecinos
+
+## 3 - Predicciones
+
+## 4 - Recomendaciones
+
+http://jarroba.com/sistemas-de-recomendacion-basados-en-filtrado-colaborativo-k-vecinos/
+https://inclass.kaggle.com/c/movie-recommendation/data
+
+ # Good to see
+ https://flowingdata.com/2013/06/21/beer-recommendation-system-in-r/
+ http://www.r-bloggers.com/simple-tools-for-building-a-recommendation-engine/
+ http://www.r-bloggers.com/recommendation-systems-in-r/
+ revoscaler package cran
+ 
+# Bibliotecas usadas:
+__recommenderlab__
---
+++ proyecto.R
@@ -0,0 +1,42 @@
+# Sys.setlocale('LC_ALL','en_GB.UTF-8')
+Sys.setlocale(locale="C")
+library("stringr")
+library("reshape2")
+sample_sub <- read.csv("data/sample_submission.csv")
+train <- read.csv("data/training_ratings_for_kaggle_comp.csv")
+movies <- readLines("data/movies.dat")
+usr <- readLines("data/users.dat")
+
+
+install = function(pkg){
+  # Si ya está instalado, no lo instala.
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, repos = "http:/cran.rstudio.com")
+    if (!require(pkg, character.only = TRUE)) stop(paste("load failure:", pkg))
+  }
+}
+install("recommenderlab")
+
+
+# Creating a DF with user's Info
+usr2 <- unlist(strsplit(usr,"::"))
+id <- usr2[seq(from = 1, to = length(usr2), by =5 )]
+df.user <- as.data.frame(id)
+df.user$sexo <- usr2[seq(from = 2, to = length(usr2), by =5 )]
+df.user$age <- usr2[seq(from = 3, to = length(usr2), by =5 )]
+df.user$ocupacion <- usr2[seq(from = 4, to = length(usr2), by =5 )]
+df.user$zip.code <- usr2[seq(from = 5, to = length(usr2), by =5 )]
+
+# MovieID::Title::Genres
+mov <- unlist(strsplit(movies,"::"))
+id <- mov[seq(from = 1, to = length(mov), by =3 )]
+df.movie <- as.data.frame(id)
+df.movie$title <- mov[seq(from = 2, to = length(mov), by =3 )]
+
+
+# delete useless id 
+train$id <- NULL
+
+
+# esta es la matriz que usaremos 
+df.train  <- dcast(train, user ~ movie)
