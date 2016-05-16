da39e57af0a0a1c7a3f27919792d9c7c49c1ede1
Miguel <miguelachof11@gmail.com>
2016-05-16 17:42:31 -0400

    calculate error

--- presentable.R
+++ presentable.R
@@ -18,7 +18,7 @@ df.user$age <- usr2[seq(from = 3, to = length(usr2), by =5 )]
 df.user$ocupacion <- usr2[seq(from = 4, to = length(usr2), by =5 )]
 df.user$zip.code <- usr2[seq(from = 5, to = length(usr2), by =5 )]
 
-#no se usan más
+#no se usan mas
 rm(usr)
 rm(usr2)
 
@@ -29,7 +29,7 @@ id <- mov[seq(from = 1, to = length(mov), by =3 )]
 df.movie <- as.data.frame(id)
 df.movie$title <- mov[seq(from = 2, to = length(mov), by =3 )]
 
-# no se usa más
+# no se usa mas
 rm(movies)
 rm(mov)
 
@@ -38,7 +38,7 @@ testing$X <- NULL
 training$X <- NULL
 
 
-# nombre de las películas
+# nombre de las pelmculas
 f <- df.movie$title[df.movie$id  %in% training$movie]
 
 # carga matriz de recomendaciones ya calculada en formato realratingmatrix
@@ -52,11 +52,30 @@ colnames(a) <- f
 # df de salida esperada en kaggle que se puede comparar con el testing 
 salida <- read.csv("data/salida.csv")
 
-
 # df.train sin nombre de película
 df.train  <- acast(training, user ~ movie)
 
-
 # carga dataframe c que contiene 10 recomendaciones por usuario
 load("data/recomendacion10.RData")
+
+
+
+
+
+
+##nuevo
+salida$real <- NULL
+
+t3 <- as.factor(training$movie)
+testing <- testing[testing$movie %in% levels(t3), ]
+df.salida  <- acast(salida, user ~ movie)
+df.test  <- acast(testing, user ~ movie)
+
+
+salida.RatingMatrix <- as(df.salida, "realRatingMatrix")
+test.RatingMatrix <- as(df.test, "realRatingMatrix")
+
+
+error <- calcPredictionAccuracy(salida.RatingMatrix, test.RatingMatrix)
+
 
